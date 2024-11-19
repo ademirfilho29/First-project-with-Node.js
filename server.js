c9f4ff080886efafd8816985ff8c08b11ce0985f
ademirfilho29 <ademirfilho38334@gmail.com>
2024-11-18 23:21:19 -0300

    Primeiro projeto em node.js

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const app = express();
+app.listen(3000,() => {
+    console.log("Servidor escutando...");
+});
+
+//Criação de rota
+app.get("/api", (rec, res) => {
+    // status code 200 significa que esta OK // 
+    res.status(200).send("rota inicial");
+});
+
+//Criação de rota => rec = requisicao, res = resposta do servidor
+app.get("/games",(rec, res) => {
+
+    //o status code tem que ser igual a 200, se for envie oq foi solicitado//
+    res.status(200).send(
+        {
+            "nome_do_jogo": "Valorant",
+            "empresa_desenvolvedora": "Riot Games",
+            "ano": 2020,
+            "genero": "FPS"
+          }
+    );
+});
+
